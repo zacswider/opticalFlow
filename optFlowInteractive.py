89d10aac3e57970653df05861d9b630ea4599f8f
Zac Swider <51267747+zacswider@users.noreply.github.com>
2021-11-23 11:57:18 -0600

    Update optFlowInteractive.py
    updated window value range

--- optFlowInteractive.py
+++ optFlowInteractive.py
@@ -5,9 +5,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-frames = list(Path('/Users/bementmbp/Desktop/bzOptFlow/imageSequence').iterdir())
+frames = list(Path('/Users/bementmbp/Desktop/testImages/eb1_crop').iterdir())
 frames.sort()
-step = 5
+step = 8
 framesToCompare = 1
 firstFrame = Image.open(frames[0])
 secondFrame = Image.open(frames[0+framesToCompare])
@@ -25,14 +25,14 @@ fig, ax = plt.subplots(figsize = (9,4.5))
 ax = [ax, ax.twinx()]
 fig.subplots_adjust(right=.7)
 
-arrows = ax[1].quiver(np.arange(0, vectors.shape[1], step), np.flipud(np.arange(vectors.shape[0], -1, -step)), 
+arrows = ax[1].quiver(np.arange(0, vectors.shape[1], step), np.flipud(np.arange(vectors.shape[0]-1, -1, -step)), 
                       vectors[::step, ::step, 0], vectors[::step, ::step, 1]*-1, color="c")
 img = ax[0].imshow(Image.open(frames[0]), cmap = "copper", aspect = "auto")
 ax[1].set_xlim(ax[0].get_xlim())
 ax[1].set_ylim(ax[0].get_ylim())
 
 winAx = fig.add_axes([.8, 0.7, 0.1, 0.05]) #rectangle of size [x0, y0, width, height]
-winValues = np.linspace(5,50,46)
+winValues = np.linspace(1,100,100)
 pyrAx = fig.add_axes([.8, 0.6, 0.1, 0.05]) #rectangle of size [x0, y0, width, height]
 pyrValues = np.linspace(0.1,0.9,9)
 levAx = fig.add_axes([.8, 0.5, 0.1, 0.05]) #rectangle of size [x0, y0, width, height]
@@ -47,7 +47,7 @@ startAx = fig.add_axes([.8, 0.1, 0.1, 0.05]) #rectangle of size [x0, y0, width, 
 startValues = np.linspace(0,len(frames),len(frames))
 
 try:
-    winSlider = Slider(ax=winAx, label='win', valmin=5, valmax=50, valinit=20, valfmt=' %0.0f Px', valstep=winValues, facecolor='#cc7000')
+    winSlider = Slider(ax=winAx, label='win', valmin=1, valmax=100, valinit=20, valfmt=' %0.0f Px', valstep=winValues, facecolor='#cc7000')
 except ValueError:
     winSlider = Slider(ax=winAx, label='test', valmin=5, valmax=50, valinit=20, valfmt=' %0.0f Px', valstep=winValues.all(), facecolor='#cc7000')
 try:
@@ -77,12 +77,19 @@ except ValueError:
 
 def update(val):
     w = int(winSlider.val)
+    print("win = ", w)
     p = pyrSlider.val
+    print("pyramid = ", p)
     l = int(levSlider.val)
+    print("levels = ", l)
     i = int(itSlider.val)
+    print("iterations = ", i)
     n = int(nSlider.val)
+    print("polyN = ", n)
     s = sSlider.val
+    print("polyS = ", s)
     f = int(startSlider.val)
+    print("frame = ", f)
     newVectors = calcFlow(frame1 = Image.open(frames[f]), frame2 = Image.open(frames[f+framesToCompare]), pyr = p, lev = l, win = w, it = i, polN = n, polS = s, flag = 1)
     arrows.set_UVC(newVectors[::step, ::step, 0], newVectors[::step, ::step, 1]*-1) 
     img.set_data(Image.open(frames[f]))  

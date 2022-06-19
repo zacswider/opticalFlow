6cd208161a65a9c4fdb52b4d365d67bbc30ed3f7
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-06-19 11:44:25 -0500

    frame skip and gauss blur sliders, 2-frame display

--- optFlowInteractive.py
+++ optFlowInteractive.py
@@ -4,18 +4,21 @@ import numpy as np
 from tkinter import Tk
 import skimage.io as skio
 import matplotlib.pyplot as plt
+from pylab import *
 from matplotlib.widgets import Slider
 from tkinter.filedialog import askopenfilename
+import scipy.ndimage as nd 
 
 '''***** Default Parameters *****'''
 #Tk().withdraw()                 
 #imagePath = askopenfilename(initialdir = './test_data')       # full path to 1-channel image
 imagePath = "/Users/bementmbp/Desktop/Scripts/opticalFlow/test_data/200102_BZ_crop.tif"    # If you want to hard code an image path
+imagePath = "/Users/bementmbp/Desktop/leslie.tif"
 imageStack=skio.imread(imagePath) 
 # scale variable for displayed vector size; bigger value = smaller vector
 scale = 1
 # step size for vectors. Larger value = less vectors displayed
-step = 4
+step = 25
 # skip frames when comparing. Default is to compare consecutive frames (i.e., skip zero)
 framesToSkip = 0
  # number of bins for the polar histogram
@@ -37,8 +40,8 @@ if imageStack.ndim > 3:
 '''***** Flow and Hist Functions *****'''
 def calcFlow(frame1, frame2, pyr, lev, win, it, polN, polS, flag):      
     # https://docs.opencv.org/2.4/modules/video/doc/motion_analysis_and_object_tracking.html#calcopticalflowfarneback
-    flow = cv.calcOpticalFlowFarneback(prev = np.array(frame1), 
-                                       next = np.array(frame2), 
+    flow = cv.calcOpticalFlowFarneback(prev = np.invert(np.array(frame1)), 
+                                       next = np.invert(np.array(frame2)), 
                                        flow = None,
                                        pyr_scale = pyr, 
                                        levels = lev,
@@ -81,6 +84,7 @@ fig = plt.figure(figsize=(10,5))
 ax1 = plt.subplot(1, 3, 1)                    
 ax2 = plt.subplot(1, 3, 2, projection='polar')
 ax3 = plt.subplot(1, 3, 3)
+
 fig.subplots_adjust(wspace = 0.5)
 
 fig.subplots_adjust(top = 0.8)        
@@ -92,7 +96,11 @@ nAx = fig.add_axes([.185, 0.825, 0.25, 0.05])
 nValues = np.linspace(1,9,5)                        
 sAx = fig.add_axes([.6, 0.9, 0.25, 0.05])           
 sValues = np.linspace(0.1,2.0,20)                   
-startAx = fig.add_axes([.6, 0.825, 0.25, 0.05])     
+startAx = fig.add_axes([.6, 0.825, 0.25, 0.05])
+skipValues = np.linspace(0,25,25)
+skipAx = fig.add_axes([.185, 0.75, 0.25, 0.05])
+gaus_values = np.linspace(0,16,16)
+gaus_ax = fig.add_axes([.6, 0.75, 0.25, 0.05])
 
 # creates sliders
 startValues = np.linspace(0 ,imageStack.shape[0], imageStack.shape[0], endpoint=False)  # sets starting frame values
@@ -100,9 +108,23 @@ winSlider = Slider(ax=winAx, label='win', valmin=1, valmax=100, valinit=20, valf
 nSlider = Slider(ax=nAx, label='polyN', valmin=1, valmax=9, valinit=7, valfmt=' %0.0f Px', valstep=nValues, facecolor='#cc7000')                                        # poly_n size slider parameters
 sSlider = Slider(ax=sAx, label='polyS', valmin=0.1, valmax=2.0, valinit=1.5, valfmt=' %1.1f Px', valstep=sValues, facecolor='#cc7000')                                  # poly_sigma size slider parameters
 startSlider = Slider(ax=startAx, label='start', valmin=0, valmax=imageStack.shape[0], valinit=0, valfmt='Frame %0.0f ', valstep=startValues, facecolor='#cc7000')       # start frame slider parameters
+skipSlider = Slider(ax=skipAx, label='skip', valmin=0, valmax=25, valinit=0, valfmt=' %0.0f frames', valstep=skipValues, facecolor='#cc7000')                           
+gaus_slider = Slider(ax=gaus_ax, label='gaus', valmin=0, valmax=16, valinit=0, valfmt=' %0.0f sigma', valstep=gaus_values, facecolor='#cc7000')
 
 # show the first frame
-imgMerge = ax1.imshow(imageStack[0], cmap = "copper", aspect = "equal") 
+
+def get_rgb(frame1, frame2):
+    def normalize(im):
+        return((im - im.min()) / (im.max() - im.min()))
+    
+    merge = np.zeros((frame1.shape[0], frame1.shape[1], 3))
+    merge[:,:,0] = normalize(frame1)
+    merge[:,:,1] = normalize(frame2)
+    merge[:,:,2] = normalize(frame1)
+
+    return(merge)
+
+imgMerge = ax1.imshow(get_rgb(firstFrame, secondFrame), aspect = "equal") 
 ax1.set_xticks([])
 ax1.set_yticks([])
 
@@ -129,7 +151,8 @@ mymean = str(round(flatmags.mean(),2))
 mystd = str(round(flatmags.std(), 2))
 text = f'Mean: {mymean} \nStd: {mystd}'
 ax3.annotate(text, xy=(x, y))
-ax3.set_ylabel('Magnitude')
+ax3.set_ylabel('Pixel shift / frame')
+ax3.set_aspect(aspect = 'equal', adjustable = 'box')
 
 '''***** Update on Sliders *****'''
 def update(val):
@@ -137,11 +160,15 @@ def update(val):
     w = int(winSlider.val)                  
     n = int(nSlider.val)
     s = sSlider.val
-    f = int(startSlider.val) 
+    f = int(startSlider.val)
+    skip = int(skipSlider.val)
+    sig = int(gaus_slider.val)
+    first = nd.gaussian_filter(imageStack[f], sigma = sig)
+    second = nd.gaussian_filter(imageStack[f+skip+1], sigma = sig)
 
     # update the flow
-    flow = calcFlow(frame1 = imageStack[f], 
-                    frame2 = imageStack[f+framesToSkip+1], 
+    flow = calcFlow(frame1 = first, 
+                    frame2 = second, 
                     pyr = 0.5, 
                     lev = 3, 
                     win = w, 
@@ -155,7 +182,7 @@ def update(val):
                         flow[::step, ::step, 1]*-1*scale)    
 
     # update images to display
-    imgMerge.set_data(imageStack[f])        # updates image to display
+    imgMerge.set_data(get_rgb(first, second))        # updates image to display
 
     # recalculate and reset new bar heights
     m, b, wi, radius = calcVectors(flow)
@@ -164,6 +191,7 @@ def update(val):
 
     # update ax3 boxplot
     ax3.cla()
+    m = m / (1 + skip)
     box = ax3.boxplot(m)
     x, y = box['medians'][0].get_xydata()[1]
     mymean = str(round(m.mean(),2))
@@ -179,5 +207,7 @@ def update(val):
 winSlider.on_changed(update)                
 nSlider.on_changed(update)                  
 sSlider.on_changed(update)                  
-startSlider.on_changed(update)              
+startSlider.on_changed(update) 
+skipSlider.on_changed(update)     
+gaus_slider.on_changed(update)        
 plt.show()

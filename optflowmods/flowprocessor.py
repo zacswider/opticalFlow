e5f7d9d340305b587c69bfde279d35a865d206ad
Zac Swider <51267747+zacswider@users.noreply.github.com>
2022-06-20 16:59:48 -0500

    Update flowprocessor.py
    functions for calculating flow and magnitudes of all pairs

--- optflowmods/flowprocessor.py
+++ optflowmods/flowprocessor.py
@@ -4,14 +4,20 @@ import numpy as np
 import scipy.ndimage as nd 
 from tifffile import imread, TiffFile
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+import time
 
 class FlowProcessor():
 
-    def __init__(self, image_path, frames_to_skip, gauss_sigma):
+    def __init__(self, image_path, win_size, polyN, polyS, frame_skip, vect_skip, gauss_sigma):
         self.image_path = image_path
+        self.win_size = win_size
+        self.polyN = polyN
+        self.polyS = polyS
+        self.frame_skip = frame_skip
+        self.vect_skip = vect_skip
         self.gauss_sigma = gauss_sigma
-        self.framesToSkip = frames_to_skip
-        self.image = imread(self.image_path)
+        self.image = imread(image_path)
 
         # standardize image dimensions
         with TiffFile(self.image_path) as tif_file:
@@ -24,6 +30,16 @@ class FlowProcessor():
                                         self.num_channels, 
                                         self.image.shape[-2], 
                                         self.image.shape[-1])
+        
+        if self.num_frames < 2:
+            print('Error: Image must have at least two frames in order to calculate flow')
+            sys.exit()
+        
+        # convert image to 8 bit
+        self.image = (((self.image - self.image.min()) / (self.image.max() - self.image.min()))*256).astype(np.uint8)
+        
+        # calculate number of sequential pairs
+        self.num_pairs = self.num_frames - self.frame_skip - 1
 
         # max project image stack if num_slices > 1
         if self.num_slices > 1:
@@ -36,35 +52,67 @@ class FlowProcessor():
                                             self.image.shape[-2], 
                                             self.image.shape[-1])
         
-        self.gauss = nd.gaussian_filter(self.image, sigma = (0,0,0,self.gauss_sigma,self.gauss_sigma))
+        self.gauss = nd.gaussian_filter(self.image[:,0,:,:,:], sigma = (0,0,self.gauss_sigma,self.gauss_sigma))
 
-    def get_flow(self):
+    def calc_mean_flow(self):
+        '''
+        Calculates the mean optical flow for every sequential pair of frames in every channel
+        in the image stack.
+        Returns a numpy array of shape (num_frames, num_channels, 2)
         '''
         
+        # empty array to fill with flow values
+        self.flow_array = np.zeros((self.num_pairs, 
+                                    self.num_channels, 
+                                    self.gauss.shape[-2], 
+                                    self.gauss.shape[-1], 
+                                    2))
+
+        # for every pair, calculates flow with specified values
+        its = self.num_pairs * self.num_channels
+        with tqdm(total = its, miniters = its/100) as pbar:
+            pbar.set_description('Calculating flow of all pairs')
+            for i in range(self.num_pairs):
+                for j in range(self.num_channels):
+                    frame1 = self.gauss[i,j,:,:]
+                    frame2 = self.gauss[i + self.frame_skip + 1,j,:,:]
+                    self.flow_array[i,j,:,:,:] = cv.calcOpticalFlowFarneback(prev = frame1, 
+                                                                             next = frame2,
+                                                                             flow = None,
+                                                                             pyr_scale = 0.5,
+                                                                             levels = 3,
+                                                                             winsize = self.win_size,
+                                                                             iterations = 3,
+                                                                             poly_n = self.polyN, 
+                                                                             poly_sigma = self.polyS, 
+                                                                             flags = 1)
+                    pbar.update(1)
+
+        return(self.flow_array)
+
+    def calc_regional_flow(self):
+        '''
+        For each array of calculated flow vectors, splits the vectors into its composite
+        magnitude and angle components, then picks the collection of highest magnitude flow
+        vectors by using either Otsu's threshold on the magnitudes array. 
         '''
 
-        def calcFlow(frame1, frame2, pyr, lev, win, it, polN, polS, flag):      
-            # https://docs.opencv.org/2.4/modules/video/doc/motion_analysis_and_object_tracking.html#calcopticalflowfarneback
-            flow = cv.calcOpticalFlowFarneback(prev = np.invert(np.array(frame1)), 
-                                               next = np.invert(np.array(frame2)), 
-                                               flow = None,
-                                               pyr_scale = pyr, 
-                                               levels = lev,
-                                               winsize = win, 
-                                               iterations = it,
-                                               poly_n = polN, 
-                                               poly_sigma = polS, 
-                                               flags = flag)
+        
+        self.mags_array = np.zeros((self.num_pairs, 
+                                    self.num_channels, 
+                                    self.gauss.shape[-2], 
+                                    self.gauss.shape[-1]))
+        
+        its = self.num_pairs * self.num_channels
+        with tqdm(total = its, miniters = its/100) as pbar:
+            pbar.set_description('Calculating regional magnitudes')
+            for i in range(self.num_pairs):
+                for j in range(self.num_channels):
+                    
+                    
 
-        # calculates flow with default values
-        flow = calcFlow(frame1 = self.gauss[0],
-                        frame2 = self.gauss[1],
-                        pyr =0.5, 
-                        lev = 3, 
-                        win = 20, 
-                        it = 3, 
-                        polN = 7, 
-                        polS = 1.5, 
-                        flag = 1) 
+                    
+                    pbar.update(1)
 
-        return(flow)
+        
+
